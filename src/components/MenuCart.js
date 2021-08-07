470099e22c8b7178747397331a44193d7fea55c2
Muhamad Amir <60350614+m-amirazmi@users.noreply.github.com>
2021-08-07 20:27:25 +0800

    pay cash

--- src/components/MenuCart.js
+++ src/components/MenuCart.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Card, Row, Col } from 'reactstrap'
+import { Card, Row, Col, Button, Input } from 'reactstrap'
 import { useCart } from '../contexts/CartContext'
 import styles from '../assets/scss/components/MenuCart.module.scss'
 
@@ -10,12 +10,28 @@ export const MenuCart = () => {
         { id: '1', name: 'cash', icon: <i className="far fa-money-bill-alt"></i> },
         { id: '2', name: 'credit card', icon: <i class="far fa-credit-card"></i> }
     ])
+    const [proceedPay, setProceedPay] = useState(false)
+    const [input, setInput] = useState({ payment: 0 })
 
     const { cart, removeFromCart } = useCart()
 
     const handleRemoveFromCart = (menu) => {
         const removedList = removeFromCart(menu)
         console.log(removedList)
+    }
+
+    const handleInput = ({ target }) => {
+        console.log(target)
+        setInput({ ...input, [target.name]: target.value })
+    }
+
+    const renderCashier = () => {
+        return (
+            <div className="ms-auto text-end">
+                <h5 className="mb-0">Muhamad Amir</h5>
+                <p className="mb-0">Cashier</p>
+            </div>
+        )
     }
 
     const renderAddedMenu = () => {
@@ -43,7 +59,7 @@ export const MenuCart = () => {
             )
         })
         return (
-            <div className="mt-5">
+            <div className="mt-2">
                 <h4 className="mb-4">Added Menu</h4>
                 <div className="p-2 border border-primary rounded">
                     <div className={`${styles.menu}`}>
@@ -58,7 +74,7 @@ export const MenuCart = () => {
         let tax = 0.00
         const subTotal = cart.reduce((acc, curr) => +curr.price + acc, 0).toFixed(2)
         tax = +(subTotal * 0.1).toFixed(2)
-        const total = +(+subTotal + +tax).toFixed(2)
+        const total = Math.round(+(+subTotal + +tax).toFixed(2))
 
         return (
             <div className="mt-5">
@@ -94,20 +110,81 @@ export const MenuCart = () => {
         })
 
         return (
-            <div className="mt-auto">
+            <div className="mt-auto mb-4">
                 <h4 className="mb-4">Payment Method</h4>
-                <Row>
+                <Row className="mb-4">
                     {paymentMethodList}
                 </Row>
+                <div>
+                    <Button className="w-100" color="primary" onClick={() => setProceedPay(!proceedPay)}>Proceed</Button>
+                </div>
             </div>
         )
     }
 
+    const renderPayCash = () => {
+        let tax = 0.00
+        const subTotal = cart.reduce((acc, curr) => +curr.price + acc, 0).toFixed(2)
+        tax = +(subTotal * 0.1).toFixed(2)
+        const total = Math.round(+(+subTotal + +tax).toFixed(2))
+
+        const balance = +input.payment - +total
+
+        return (
+            <div className="mt-auto mb-4">
+                <div className="mb-2 d-flex flex-row align-items-center text-primary" onClick={() => setProceedPay(!proceedPay)} style={{ cursor: 'pointer' }}>
+                    <i className="fas fa-long-arrow-alt-left me-2"></i>
+                    <span>Back</span>
+                </div>
+                <div className="mb-3">
+                    <h4>Pay with Cash</h4>
+                    <small>Please make sure to ask customer for exact change.</small>
+                </div>
+                <Row className="d-flex align-items-center mb-3">
+                    <Col md={6}>
+                        <p className="m-0 fs-4">Customer pays:</p>
+                    </Col>
+                    <Col md={6} className="d-flex align-items-center fs-4 fw-bold">
+                        <p className="m-0 me-2">RM</p>
+                        <Input type="number" className={`${styles.input} p-0 fs-4 border-primary fw-bold text-center`} name="payment" id="cash" placeholder="eg: 10.50" step=".10" value={input.payment} onChange={handleInput} />
+                    </Col>
+                </Row>
+
+                <Row className="d-flex align-items-center">
+                    <Col md={6}>
+                        <p className="m-0 fs-4">Balance:</p>
+                    </Col>
+                    <Col md={6} className="d-flex align-items-center fs-4 fw-bold">
+                        <p className="m-0 me-2">RM</p>
+                        <Input type="number" disabled className={`bg-white border-0 p-0 fs-4 fw-bold text-center`} value={balance.toFixed(2)} />
+                    </Col>
+                </Row>
+
+                <div className="mt-3">
+                    <Button className="w-100" color="primary">Confirm</Button>
+                </div>
+
+            </div>
+        )
+    }
+
+    const renderPaymentSwitch = () => {
+
+        if (proceedPay && selectedPaymentMethod.id === '1') return renderPayCash()
+        if (proceedPay && selectedPaymentMethod.id === '2') return (
+            <div>
+                This is credit card pay
+            </div>
+        )
+        if (!proceedPay) return renderPaymentMethod()
+    }
+
     return (
         <div className="p-4 d-flex flex-column h-100">
+            {renderCashier()}
             {renderAddedMenu()}
             {renderTotal()}
-            {renderPaymentMethod()}
+            {renderPaymentSwitch()}
         </div>
     )
 }
